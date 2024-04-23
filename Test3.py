e3af665aa6c475fc38b660eb804a63beb98feb60
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2024-04-23 09:31:06 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -272,13 +272,13 @@ def main():
     league = st.selectbox('Choose an option:', ['IPL','PSL','SA20','T20I (test playing nations only)'])
     # Load your data
     if league == 'IPL':
-        data =  pd.read_csv('all_matches.csv',low_memory=False)
+        data =  load_data('all_matches.csv')
     elif league == 'PSL':
-        data =  pd.read_csv('PSL.csv',low_memory=False)
+        data =  load_data('PSL.csv')
     elif league == 'SA20':
-        data =  pd.read_csv('SA20.csv',low_memory=False)
+        data =  load_data('SA20.csv')
     elif league == 'T20I (test playing nations only)':
-        data =  pd.read_csv('testplayingnations.csv',low_memory=False)
+        data =  load_data('testplayingnations.csv')
     data['B'] = 1
 
     # Set 'B' to 0 for deliveries that are wides
