4c1a6115864400843116c863ec6a2dc360488171
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2024-04-26 16:12:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -181,6 +181,7 @@ def main():
             d = pd.read_csv(dic[l], low_memory=False)
             leagues.append(d)
         data =pd.concat(leagues, ignore_index=True)
+        print(data)
         data = data.drop_duplicates()
         # # Load your data
         # if league == 'IPL':
